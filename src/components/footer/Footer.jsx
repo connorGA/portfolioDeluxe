df4d446f8440f9bef8b8e5a85dc2643f1f051462
connorGA <ronnoccap@gmail.com>
2023-01-09 16:30:11 -0800

    chore: removed insta link, updated resume

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
       <div className="footer__socials">
         <a href="https://www.linkedin.com/in/connor-cappello/" target='_blank'><BsLinkedin/></a>
         <a href="https://github.com/connorGA" target='_blank'><FaGithub/></a>
-        <a href="https://www.instagram.com/cj_cappello/" target='_blank'><TfiInstagram/></a>
+        {/* <a href="https://www.instagram.com/cj_cappello/" target='_blank'><TfiInstagram/></a> */}
       </div>
 
       <div className="footer__copyright">
--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CV from '../../assets/Software Engineering Resume (1).pdf'
+import CV from '../../assets/Software Engineering Resume (2).pdf'
 
 const CTA = () => {
   return (
